960eec73573754877e2dfc65689a1ba46ab04e44
Daniel <dannyblaze469@gmail.com>
2024-03-23 15:48:46 -0700

    Initial Commit

---
+++ BB Quotes/BB_QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BB_QuotesApp.swift
+//  BB Quotes
+//
+//  Created by Daniel Ezekiel on 3/23/24.
+//
+
+import SwiftUI
+
+@main
+struct BB_QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
